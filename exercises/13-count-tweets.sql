804b72238cd8d9baa48dbc9e1123fe8960cbd7d8
Chezkah Kate Tahadlangit <cftahadlangit@up.edu.ph>
2017-12-20 23:52:06 +0800

    Delete 13-count-tweets.sql

--- exercises/13-count-tweets.sql
+++
@@ -1,9 +0,0 @@
--- Count how many tweets the given user has created.
---
--- You have access to the following variables which can be used as placeholders
--- for actual values:
---     - email
---
--- Write your query below:
-
-SELECT COUNT (user_email) FROM tweets where user_email = '{{email}}'
